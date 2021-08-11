e26356c87c08ac0045c69d53b7acdc6e626cf095
OnWhite <63966472+OnWhite@users.noreply.github.com>
2021-08-11 13:34:54 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,12 +1,40 @@
 # Lydos
 A script making Rhea input files form your file explorer
 
-
-# Idea
-In order to use Rhea you need to remultiplex your fastq files.
+### Idea
+In order to use Rhea you need to multiplex your fastq files.
 Rhea provides a perlscript with this capability for you, but it
 needs a .tab file including the filenames needed 
 to do so. This script automatizes the process of making this file.
 
+### Usage
+To use this script you need to enter
 
-# Usage 
+    - the filename of the .tab file 
+    - from where automatically generated ids should start 
+        NOTE: the ID value has to be a whole number,
+                the first ID will have the number selected 
+                the next IDs will ascend by one
+    - whether you want a FilesNotMultiplexed.tab file, 
+        it will list all the files and folders in your input folder that aren't in the outputfile 
+    - where you want to save the file 
+    - whether your data is paired or not, if so pairs need to have R1 and R2 in the filename,
+        if not it needs to include R1 in the filenames 
+
+then click the "Read Files" Button select the location of your fasta /fastq files
+
+### Output
+The Output will consist of your .tab file with the list of your filename pairs, and if selected so a file called FilesNotMultiplexed.tab.
+
+### Defaultvalues
+The defaultvalues are:
+
+- filename:                 multiplexing_table.tab
+- IDs:                      none
+- FilesNotMultiplexed:      none
+- Output Folder:            Input folder
+- paired:                   no
+
+### Furthure usage
+In order to run the perlscript you need to move the multiplexing_table.tab to the dir in which the perlscript remultiplexor-paired.pl is located.
+Note that there shouldn't be several multiplexing_table.tab files in the directory. Now just apply the perlscript according to it's  ReadMe.
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import pathlib
 import shutil
-import tkinter as tk
 import os
+import sys
 from tkinter.filedialog import askdirectory, Tk
+from kivy.core.window import Window
 from kivy.lang import Builder
 from kivymd.app import MDApp
 
-# graphic string
+# graphic String
 KV = '''
 Screen:
         canvas.before:
@@ -16,16 +18,7 @@ Screen:
                         pos: self.pos
                         size: self.size
                     
-        RelativeLayout:
-                #topprogressbar
-                MDProgressBar:
-                        orientation: "horizontal"
-                        type: "determinate"
-                        running_duration: 1
-                        color: (93/255,160/255,161/255,255/255)
-                        pos_hint: {'center_x': 0.5, 'center_y': 1}
-                        size: self.size
-                        value: app.getprogress()
+        RelativeLayout:                       
                 #textfield for filename 
                 MDTextField:
                         id:name
@@ -36,75 +29,99 @@ Screen:
                         line_color_focus: (93/255,160/255,161/255,255/255)
                         color_mode: 'custom'
                         #mode:"rectangle"
-                        current_hint_text_color:(93/255,160/255,161/255,255/255)
+                        current_hint_text_color:(93/255,160/255,161/255,255/255)          
                         
-                # checkbox for automatised ids
+
+                #Textfiled for the starting ID
+                MDTextField:
+                        id:name2
+                        hint_text: "ID starting number"
+                        pos_hint: {'center_x': 0.5, 'center_y': 0.8}
+                        size_hint: 0.3, 0.1
+                        line_color_focus: (93/255,160/255,161/255,255/255)
+                        color_mode: 'custom'
+                        current_hint_text_color:(93/255,160/255,161/255,255/255)
+                #checkbox whether files are paired 
                 CheckBox:
-                        pos_hint: {'center_x': 0.365, 'center_y': 0.8}
-                        on_active: app.setTrue()
-                        #color: (93/255,160/255,161/255,255/255)
-                        size_hint_x: .20
-               #label describing the ids checkbox
+                        pos_hint: {'center_x': 0.52, 'center_y': 0.6}
+                        on_active: app.setTruepaired()
+                        size_hint_x: .10
+                        size: dp(48), dp(48)
+                #label describing the paired checkbox
                 Label:
-                        pos_hint: {'center_x': 0.51, 'center_y': 0.8}
-                        text: "IDs"
+                        pos_hint: {'center_x': 0.59, 'center_y':  0.6}
+                        text: "paired"
                         size_hint_x: .80
                         font_size:20
-                #foleder to select the output place
+                #checkbox for file listing not multiplexed files
+                CheckBox:
+                        pos_hint: {'center_x': 0.365, 'center_y': 0.7}
+                        on_active: app.setTrueFilesNotMultiplexed()
+                        #color: (93/255,160/255,161/255,255/255)
+                        size_hint_x: .10
+                        size: dp(48), dp(48)
+               #label describing file listing not multiplexed files checkbox
+                Label:
+                        pos_hint: {'center_x': 0.51, 'center_y': 0.7}
+                        text: "FilesNotMultiplexed"
+                        size_hint_x: .80
+                        font_size:20
+                #folder to select the output place
                 MDIconButton:
                         id: button1
                         icon:'folder'
-                        pos_hint: {'center_x': 0.365, 'center_y': 0.7}
+                        pos_hint: {'center_x': 0.365, 'center_y': 0.6}
                         text_color: (93/255,160/255,161/255,255/255)
                         theme_text_color: "Custom"
                         markup: False
                         on_release: app.setSaveplace()
                 
-                #button starting the read process of the selected folder
+                #button starting the reading process of the selected folder
                 MDRaisedButton:
-                        pos_hint: {'center_x': 0.412, 'center_y': 0.6}
+                        pos_hint: {'center_x': 0.412, 'center_y': 0.5}
                         id: auslesen
                         md_bg_color: (93/255,160/255,161/255,255/255)
                         theme_text_color: "Custom"
                         markup: False
                         text: "Read Files"
                         pos: (200,200)
-                        on_release: app.auslesen()
+                        on_release: app.readout()
                 #button to open the manual/description
                 MDRaisedButton:
                         id: reset
-                        pos_hint: {'center_x': 0.547, 'center_y': 0.6}
+                        pos_hint: {'center_x': 0.575, 'center_y': 0.5}
                         color: (154/255,84/255,93/255,200/255)
-                        text: "ReadMe"
+                        text: "Exit"
                         md_bg_color: (93/255,160/255,161/255,255/255)
                         theme_text_color: "Custom"
                         markup: True
+                        on_release: app.exit()
+                Label:
+                        id: name_label
+                        pos_hint: {'center_x': 0.5, 'center_y': 0.2}
+                        text: ""
+                        font_size: 16
                         
                         
         '''
 
-class rheascript(MDApp):
+
+class Lydos(MDApp):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
         # setting global variables with default values
-        global IDs
-        IDs=False
+        global paired
+        paired = False
+        global FNMdat
+        FNMdat = False
         global savefile
-        savefile=""
+        savefile = ""
         # load the Kivy String to start the UI
         self.screen = Builder.load_string(KV)
 
-
-
-    def getprogress(self):
-        # progressbar showing when the programm is finished
-        global progress
-        progress=20
-        return progress
-
-
-
-    def browseFiles(self):
+    @staticmethod
+    def browseFiles():
         # a method showing a file explorer window to select files
         # draw the explorer (tkinter)
         Tk().withdraw()
@@ -114,117 +131,182 @@ class rheascript(MDApp):
         global selectedfile
         selectedfile = filename
 
-    def setTrue(self):
-        # method showing which value the IDs checkbox has
+    @staticmethod
+    def setTruepaired():
+        # method showing which value the paired checkbox has
         # runs when the box in the UI is checked
-        global IDs
-        if IDs==False:
+        global paired
+        if not paired:
             # when checkbox starts with an unchecked box set it as checked
-            IDs=True
+            paired = True
         else:
             # checkbox starts with an checked box set it als unchecked
-            IDs = False
+            paired = False
+
+    @staticmethod
+    def exit():
+        # kill the Mainwindow
+        sys.exit()
+
+    @staticmethod
+    def setTrueFilesNotMultiplexed():
+        # method showing which value the FilesNotMultiplexed checkbox has
+        # runs when the box in the UI is checked
+        global FNMdat
+        if not FNMdat:
+            # when checkbox starts with an unchecked box set it as checked
+            FNMdat = True
+        else:
+            # checkbox starts with an checked box set it als unchecked
+            FNMdat = False
 
     def Files(self):
         # Method to make the .tab table for rhea
-        if selectedfile!= "":
+        if selectedfile != "":
             directories = os.listdir(selectedfile)
             # creating a .tab table with the selected name default is "samples-sequences"
-            if self.screen.ids.name.text=="":
-                mainfile = open("samples-sequences.tab", "w+")
+            global fehlermeldung
+            filename = self.screen.ids.name.text + ".tab"
+            if len(str(filename).strip("/\\: * ?\"<>|")) != len(str(filename)):
+                self.screen.ids.name_label.text = "Your Filename shouldn't contain  / \\ : * ? \" < > |"
+                return ""
+            # creating a .tab table with the selected name default is "samples-sequences"
+            if self.screen.ids.name.text == "":
+                filename = "multiplexing_table.tab"
+
+            mainfile = open(filename, "w+")
+
+            if not paired:
+                # creating the first line following the rhea syntax
+                mainfile.write("#Sample_ID\tR1-Ilumina-Output\n")
+
+                line = []
+
+                # running through the dir to find R1 and R2 pairs
+                for file in directories:
+                    if file.__contains__("R1"):
+                        # add string line to list
+                        line.append("\t" + file + "\t")
             else:
-                mainfile = open( self.screen.ids.name.text+".tab", "w+")
+                # creating the first line following the rhea syntax
+                mainfile.write("#Sample_ID\tR1-Ilumina-Output\tR2-Ilumina-Output\n")
 
-            # creating the first line following the rhea syntax
-            mainfile.write("#Sample_ID\tR1-Ilumina-Output\tR2-Ilumina-Output\n")
+                line = []
 
+                # running through the dir to find R1 and R2 pairs
+                for file in directories:
+                    for file2 in directories:
+                        # if the dat name exept the R1 or R2 are the same
+                        if file.replace("R1", "").replace("R2", "") == file2.replace("R2", "").replace("R1", ""):
+                            # if the two files are the of the same pair
+                            if file.__contains__("R1") and file2.__contains__("R2"):
+                                # add string line to list
+                                line.append("\t" + file + "\t" + file2 + "\t")
+            if FNMdat:
+                # creating a file where all files that are not multiplexed are saved
+                notinmainfilefile = open("FilesNotMultiplexed.tab", "w+")
+                # iterating through the dir chosen
+                for file in directories:
+                    f = 0
+                    # if filename is inside the list of paired files
+                    for i in line:
+                        if i.__contains__(file):
+                            # set a bookmark
+                            f = 1
+                    if f == 0:
+                        # if no bookmark found -> save in the filesNotMultiplexed.tab file
+                        notinmainfilefile.write(file + "\n")
+                notinmainfilefile.close()
 
-            line = []
+            if self.screen.ids.name2.text != "":
+                if not self.screen.ids.name2.text.isnumeric():
+                    self.screen.ids.name_label.text = "Your ID is not a whole number"
+                    return ""
 
-            #running through the dir to find R1 and R2 pairs
-            for file in directories:
-                for file2 in directories:
-                    # if the dat name exept the R1 or R2 are the same
-                    if file.replace("R1","").replace("R2","")==file2.replace("R2","").replace("R1",""):
-                        # if the two files are the of the same pair
-                        if file.__contains__("R1") and file2.__contains__("R2"):
-                            # add string line to list
-                            line.append("\t"+file+"\t"+file2+"\t")
-
-            # creating a file where all files that are not multiplexed are saved
-            notinmainfilefile = open("FilesNotMultiplexed.tab", "w+")
-            # iterating through the dir chosen
-            for file in directories:
-                f=0
-                # if filename is inside the list of paired files
-                for i in line:
-                    if i.__contains__(file):
-                        # set a bookmark
-                        f=1
-                if f==0:
-                    # if no bookmark found -> save in the filesNotMultiplexed.tab file
-                    notinmainfilefile.write(file+"\n")
-
-
-            if IDs==True:
                 # if the user wants automated ids
-                f=0
+                f = int(self.screen.ids.name2.text)
                 # counter for the id indexes
+                if len(line) < 2:
+                    # Error
+                    self.screen.ids.name_label.text = "There aren't enough R1/R2 (paired) files"
+                    return ""
+
                 for i in line:
-                    mainfile.write(str(f) +i+"\n")
+                    mainfile.write(str(f) + i + "\n")
                     # writing in the .tab outputfile
-                    f=f+1
+                    f = f + 1
 
             else:
+                if len(line) < 2:
+                    # Error
+                    self.screen.ids.name_label.text = "There aren't enough R1/R2 (paired) files"
+                    return ""
                 # without ids
                 for i in line:
-                    mainfile.write( i+"\n")
+                    mainfile.write(i + "\n")
             # close files
             mainfile.close()
-            notinmainfilefile.close()
 
-            if savefile=="":
-                # save mainfile and notinmainfilefile at the inputfiles' place
-                shutil.move("C:\\Users\\mitarbeiter\\PycharmProjects\\rheascript\\" + self.screen.ids.name.text +".tab", selectedfile.replace("/", "\\"))
-                shutil.move("C:\\Users\\mitarbeiter\\PycharmProjects\\rheascript\\FilesNotMultiplexed.tab", selectedfile.replace("/", "\\"))
+            if savefile == "":
+                try:
+                    # save mainfile and notinmainfilefile at the inputfiles' place
+                    shutil.move(str(pathlib.Path("test.py").parent.absolute()) + "\\" + filename,
+                                selectedfile.replace("/", "\\"))
+                except IOError:
+                    # Error
+                    self.screen.ids.name_label.text = "The file " + filename + " already exists"
+                    os.remove(filename)
+                    return ""
+                if FNMdat:
+                    try:
+                        shutil.move(str(pathlib.Path("test.py").parent.absolute()) + "\\FilesNotMultiplexed.tab",
+                                    selectedfile.replace("/", "\\"))
+                    except IOError:
+                        # Error
+                        self.screen.ids.name_label.text = "The file FilesNotMultiplexed.tab already exists"
+                        os.remove("FilesNotMultiplexed.tab")
+                        return ""
             else:
-                # save at the selected place
-                shutil.move("C:\\Users\\mitarbeiter\\PycharmProjects\\rheascript\\"+self.screen.ids.name.text+".tab", savefile.replace("/","\\"))
-                shutil.move(
-                    "C:\\Users\\mitarbeiter\\PycharmProjects\\rheascript\\FilesNotMultiplexed.tab",
-                    savefile.replace("/","\\"))
+                try:
+                    # save at the selected place
+                    shutil.move(str(pathlib.Path("test.py").parent.absolute()) + "\\" + filename,
+                                savefile.replace("/", "\\"))
+                except IOError:
+                    # Error
+                    self.screen.ids.name_label.text = "The file " + filename + " already exists"
+                    os.remove(filename)
+                    return ""
+                if FNMdat:
+                    try:
+                        shutil.move(str(pathlib.Path("test.py").parent.absolute()) +
+                                    "\\FilesNotMultiplexed.tab",
+                                    savefile.replace("/", "\\"))
+                    except IOError:
+                        # Error
+                        self.screen.ids.name_label.text = "The file FilesNotMultiplexed.tab already exists"
+                        os.remove("FilesNotMultiplexed.tab")
+                        return ""
+            self.screen.ids.name_label.text = "Your File(s) are ready"
 
-
+            return ""
 
     def build(self):
+        Window.size = (800, 500)
+        Window.set_icon('archive-search-outline.png')
+        self.icon = "archive-search-outline.png"
         return self.screen
 
-    def Fehlermeldung(self):
-        #never used
-        window = tk.Tk()
-        window.title("Error")
-        fehler = tk.Label(window)
-        fehler.place(x=0, y=0)
-        fehler.pack()
-        window.mainloop()
-
-    def einlesen(self):
-        self.browseFiles()
-
     def setSaveplace(self):
+        # method to set the place for the outputfile
         self.browseFiles()
         global savefile
-        savefile=selectedfile
+        savefile = selectedfile
 
-    def auslesen(self):
+    def readout(self):
+        # start browser/ main algorithm
         self.browseFiles()
         self.Files()
 
 
-
-
-# main
-# mainUi
-
 if __name__ == '__main__':
-    rheascript().run()
+    Lydos().run()
